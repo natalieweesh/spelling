f0080ec8a7cb99d5313d63f8f14ca7796f90b10c
Natalie Wee <natalieweesh@gmail.com>
2020-07-18 19:11:44 -0400

    make the spelling bee practice app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import words from './words/words.json';
 import './App.css';
 
+const TOTAL = 178691;
+
 function App() {
+  const timer = useRef(null);
+  const textBox = useRef(null);
+  const newWordButton = useRef(null);
+  const [answer, setAnswer] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [time, setTime] = useState(0);
+  const [word, setWord] = useState('');
+  const [counting, setCounting] = useState(false);
+  const [timesUp, setTimesUp] = useState(false);
+
+  const speak = (words, timeout = 0) => {
+    setTimeout(() => {
+      window.responsiveVoice.speak(words)
+    }, timeout)
+  }
+
+  const generateWord = () => {
+    const rand = Math.floor(Math.random() * TOTAL);
+    const word = words[rand];
+    setWord(word);
+    console.log('word', word);
+    setTime(30);
+    // setTime(5);
+    setCounting(true);
+    speak(word);
+  }
+
+  const checkAnswer = () => {
+    return answer.trim().toLowerCase() === word;
+  }
+
+  useEffect(() => {
+    if (counting) {
+      if (time === 0) {
+        setCounting(false);
+        clearTimeout(timer.current);
+        setTime(0);
+        setSubmitted(true);
+        setTimesUp(true);
+        setTimeout(() => {
+          newWordButton.current.focus();
+        }, 50)
+      } else {
+        timer.current = setTimeout(() => {
+          setTime(time - 1);
+        }, 1000)
+      }
+    }
+  }, [time, counting])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App-header">
+      <header>
+        <p className="title">Spelling Bee Practice!</p>
       </header>
+      <div className="main">
+        <div className="timerow">
+          <div className="timesup">{timesUp && 'time\'s up'}</div>
+          {counting && <div className="time">{time}</div>}
+        </div>
+        {!counting && <button ref={newWordButton} disabled={counting} onClick={(e) => {
+          generateWord();
+          setSubmitted(false);
+          setTimesUp(false);
+          setAnswer('');
+          textBox.current.focus();
+          console.log()
+        }}>Gimme a word!</button>}
+        {counting && <button onClick={() => {
+          speak(word);
+          textBox.current.focus();
+        }}>Say it again</button>}
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          if (answer.trim().length === 0 || !counting) {
+            console.log(
+              'no length', word.trim().length
+            )
+            return;
+          }
+          setSubmitted(true);
+          console.log('answer', answer)
+          clearTimeout(timer.current);
+          setCounting(false);
+          setTime(0);
+          setTimeout(() => {
+            newWordButton.current.focus();
+          }, 50)
+        }}>
+          <input ref={textBox} type="text" value={answer} onChange={(e) => setAnswer(e.target.value)} />
+          <button disabled={submitted} type="submit">OK</button>
+        </form>
+        <div className="feedback">
+          <div className={checkAnswer() ? 'right' : 'wrong'}>{submitted && (checkAnswer() ? 'good job' : 'oops! here\'s the correct spelling:')}</div>
+          <div>{submitted && !checkAnswer() && word}</div>
+        </div>
+      </div>
     </div>
   );
 }
